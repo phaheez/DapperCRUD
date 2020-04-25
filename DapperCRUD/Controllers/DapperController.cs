7d53a2890bf5a33c0834bc8f98a638e2f4c2a191
Fasasi Faiz <faizfasasi@hotmail.com>
2020-04-25 21:59:51 +0100

    Add project files.

---
+++ DapperCRUD/Controllers/DapperController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using DapperCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperCRUD.Controllers
+{
+    public class DapperController : Controller
+    {
+        public IActionResult Index()
+        {
+            var records = DapperORM.ReturnList<Login>("LoginViewAll");
+            return View(records);
+        }
+
+        [HttpGet]
+        public IActionResult AddOrEdit(int? id)
+        {
+            if (id != null)
+            {
+                var param = new DynamicParameters();
+                param.Add("@Id", id);
+
+                var record = DapperORM.ReturnList<Login>("LoginViewById", param).FirstOrDefault<Login>();
+                return View(record);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddOrEdit([Bind(include: "Id, Email, Password")]Login model)
+        {
+            var param = new DynamicParameters();
+            param.AddDynamicParams(new
+            {
+                @Id = model.Id,
+                @Email = model.Email,
+                @Password = model.Password,
+            });
+            //param.Add("@Id", model.Id);
+            //param.Add("@Email", model.Email);
+            //param.Add("@Password", model.Password);
+
+            DapperORM.ExecuteWithoutReturn("LoginAddOrEdit", param);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var param = new DynamicParameters();
+            param.Add("@Id", id);
+
+            DapperORM.ExecuteWithoutReturn("LoginDeleteById", param);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DapperCRUD/Models/DapperORM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace DapperCRUD.Models
+{
+    public static class DapperORM
+    {
+        private static string connectionString = @"Data Source=FASASI-FAIZ\SQLEXPRESS; Initial Catalog=DapperCRUD; Integrated Security=True;";
+
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (var sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (var sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.ExecuteScalar(procedureName, param, commandType: CommandType.StoredProcedure), typeof(T));
+            }
+        }
+
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (var sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
